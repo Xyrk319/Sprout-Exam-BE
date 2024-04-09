8f956317e78681895b3922d71a1f105780fcb3e3
Jonahlee Xyrkcysz Fabula <xyrk.fabula@dice205.com>
2024-04-09 11:58:09 +0800

    fix: cors and login via email

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from app.database_conn import engine, SessionLocal
 from app.models import UserModel, RoleModel 
@@ -9,6 +10,15 @@ RoleModel.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(auth.router)
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
